60ddff8db47a06ec5f2342728750bba95dca4a93
Matt Onimus <mattonimus@gmail.com>
2020-12-12 16:27:45 -0500

    working on ch3

---
+++ server_time.js
@@ -0,0 +1,15 @@
+
+
+
+
+require("http").createServer((inRequest, inResponse) => {
+  const requestModule = require("request");
+  requestModule(
+    "http://worldtimeapi.org/api/timezone/America/New_York",
+    function(inErr, inResp, inBody) {
+      inResponse.end(
+        `Hello from my first Node Web Server: ${inBody}`
+      );
+    }
+  );
+}).listen(80);
